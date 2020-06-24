cd872e5a5e45378383a82de4600e92b836ccef29
Vadim Nifadyev <vnifadyev@gmail.com>
2020-06-24 10:28:58 +0300

    Add dropdown menu with logout logic

--- django_test_1/django_test_1/urls.py
+++ django_test_1/django_test_1/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, re_path
+from django.contrib.auth import views as auth_views
 
 from accounts import views as accounts_views
 from boards import views
@@ -25,6 +26,7 @@ from boards import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('signup/', accounts_views.signup, name='signup'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     re_path(r'^boards/(?P<pk>\d+)/$', views.board_topics, name='board_topics'),
     re_path(r'^boards/(?P<pk>\d+)/new/$', views.new_topic, name='new_topic'),
     path('admin/', admin.site.urls),
