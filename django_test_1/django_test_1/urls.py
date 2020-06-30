36ea1090cb2a6cdc880ebc315c927bb3c8069b0c
Vadim Nifadyev <vnifadyev@gmail.com>
2020-06-30 17:08:58 +0300

    Add reply for topics

--- django_test_1/django_test_1/urls.py
+++ django_test_1/django_test_1/urls.py
@@ -69,5 +69,10 @@ urlpatterns = [
     re_path(r'^boards/(?P<pk>\d+)/new/$', views.new_topic, name='new_topic'),
     re_path(
         r'^boards/(?P<pk>\d+)/topics/(?P<topic_pk>\d+)/$', views.topic_posts, name='topic_posts'),
+    re_path(
+        r'^boards/(?P<pk>\d+)/topics/(?P<topic_pk>\d+)/reply/$',
+        views.reply_topic,
+        name='reply_topic'
+    ),
     path('admin/', admin.site.urls),
 ]
