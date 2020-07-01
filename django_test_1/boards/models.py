139261d3b68f61846a81e4f163a61fb0b54a1dad
Vadim Nifadyev <vnifadyev@gmail.com>
2020-07-01 16:41:38 +0300

    Add markdown support and SimpleMDE integration

--- django_test_1/boards/models.py
+++ django_test_1/boards/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.utils.text import Truncator
 from django.contrib.auth.models import User
+from django.utils.html import mark_safe
+from markdown import markdown
 
 
 class Board(models.Model):
@@ -17,6 +19,7 @@ class Board(models.Model):
     def get_last_post(self):
         return Post.objects.filter(topic__board=self).order_by('-created_at').first()
 
+
 class Topic(models.Model):
     subject = models.CharField(max_length=255)
     last_updated = models.DateTimeField(auto_now_add=True)
@@ -27,7 +30,6 @@ class Topic(models.Model):
 
     def __str__(self):
         return self.subject
-
 
 
 class Post(models.Model):
@@ -42,3 +44,6 @@ class Post(models.Model):
     def __str__(self):
         truncated_message = Truncator(self.message)
         return truncated_message.chars(30)
+
+    def get_message_as_markdown(self):
+        return mark_safe(markdown(self.message, safe_mode='escape'))
