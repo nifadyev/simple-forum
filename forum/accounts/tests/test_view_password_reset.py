ce10a6ffbe17455978639d2b69275012b794cfc8
Vadim Nifadyev <vnifadyev@gmail.com>
2020-09-07 21:58:05 +0300

    Refactor accounts tests
    - Remove usage of deprecated assertEquals
    - Replace ''' with """ in docstrings

--- forum/accounts/tests/test_view_password_reset.py
+++ forum/accounts/tests/test_view_password_reset.py
@@ -15,23 +15,23 @@ class PasswordResetTests(TestCase):
         self.response = self.client.get(url)
 
     def test_status_code(self):
-        self.assertEquals(self.response.status_code, 200)
+        self.assertEqual(self.response.status_code, 200)
 
     def test_view_function(self):
         view = resolve('/reset/')
-        self.assertEquals(view.func.view_class, auth_views.PasswordResetView)
+
+        self.assertEqual(view.func.view_class, auth_views.PasswordResetView)
 
     def test_csrf(self):
         self.assertContains(self.response, 'csrfmiddlewaretoken')
 
     def test_contains_form(self):
         form = self.response.context.get('form')
+
         self.assertIsInstance(form, PasswordResetForm)
 
     def test_form_inputs(self):
-        '''
-        The view must contain two inputs: csrf and email
-        '''
+        """The view must contain two inputs: csrf and email."""
         self.assertContains(self.response, '<input', 2)
         self.assertContains(self.response, 'type="email"', 1)
 
@@ -44,10 +44,9 @@ class SuccessfulPasswordResetTests(TestCase):
         self.response = self.client.post(url, {'email': email})
 
     def test_redirection(self):
-        '''
-        A valid form submission should redirect the user to `password_reset_done` view
-        '''
+        """A valid form submission should redirect the user to `password_reset_done` view."""
         url = reverse('password_reset_done')
+
         self.assertRedirects(self.response, url)
 
     def test_send_password_reset_email(self):
@@ -60,11 +59,9 @@ class InvalidPasswordResetTests(TestCase):
         self.response = self.client.post(url, {'email': 'donotexist@email.com'})
 
     def test_redirection(self):
-        '''
-        Even invalid emails in the database should
-        redirect the user to `password_reset_done` view
-        '''
+        """Invalid emails in the database should redirect the user to `password_reset_done` view."""
         url = reverse('password_reset_done')
+
         self.assertRedirects(self.response, url)
 
     def test_no_reset_email_sent(self):
@@ -77,46 +74,40 @@ class PasswordResetDoneTests(TestCase):
         self.response = self.client.get(url)
 
     def test_status_code(self):
-        self.assertEquals(self.response.status_code, 200)
+        self.assertEqual(self.response.status_code, 200)
 
     def test_view_function(self):
         view = resolve('/reset/done/')
-        self.assertEquals(view.func.view_class, auth_views.PasswordResetDoneView)
+        self.assertEqual(view.func.view_class, auth_views.PasswordResetDoneView)
 
 
 class PasswordResetConfirmTests(TestCase):
     def setUp(self):
+        """Create a valid password reset token based on how django creates the token internally."""
         user = User.objects.create_user(username='john', email='john@doe.com', password='123abcdef')
-
-        '''
-        create a valid password reset token
-        based on how django creates the token internally:
-        https://github.com/django/django/blob/1.11.5/django/contrib/auth/forms.py#L280
-        '''
         self.uid = urlsafe_base64_encode(force_bytes(user.pk))
         self.token = default_token_generator.make_token(user)
-
         url = reverse('password_reset_confirm', kwargs={'uidb64': self.uid, 'token': self.token})
         self.response = self.client.get(url, follow=True)
 
     def test_status_code(self):
-        self.assertEquals(self.response.status_code, 200)
+        self.assertEqual(self.response.status_code, 200)
 
     def test_view_function(self):
-        view = resolve('/reset/{uidb64}/{token}/'.format(uidb64=self.uid, token=self.token))
-        self.assertEquals(view.func.view_class, auth_views.PasswordResetConfirmView)
+        view = resolve(f'/reset/{self.uid}/{self.token}/')
+
+        self.assertEqual(view.func.view_class, auth_views.PasswordResetConfirmView)
 
     def test_csrf(self):
         self.assertContains(self.response, 'csrfmiddlewaretoken')
 
     def test_contains_form(self):
         form = self.response.context.get('form')
+
         self.assertIsInstance(form, SetPasswordForm)
 
     def test_form_inputs(self):
-        '''
-        The view must contain two inputs: csrf and two password fields
-        '''
+        """The view must contain two inputs: csrf and two password fields."""
         self.assertContains(self.response, '<input', 3)
         self.assertContains(self.response, 'type="password"', 2)
 
@@ -127,9 +118,7 @@ class InvalidPasswordResetConfirmTests(TestCase):
         uid = urlsafe_base64_encode(force_bytes(user.pk))
         token = default_token_generator.make_token(user)
 
-        '''
-        invalidate the token by changing the password
-        '''
+        # Invalidate the token by changing the password
         user.set_password('abcdef123')
         user.save()
 
@@ -137,12 +126,13 @@ class InvalidPasswordResetConfirmTests(TestCase):
         self.response = self.client.get(url)
 
     def test_status_code(self):
-        self.assertEquals(self.response.status_code, 200)
+        self.assertEqual(self.response.status_code, 200)
 
     def test_html(self):
         password_reset_url = reverse('password_reset')
+
         self.assertContains(self.response, 'invalid password reset link')
-        self.assertContains(self.response, 'href="{0}"'.format(password_reset_url))
+        self.assertContains(self.response, f'href="{password_reset_url}"')
 
 
 class PasswordResetCompleteTests(TestCase):
@@ -151,8 +141,9 @@ class PasswordResetCompleteTests(TestCase):
         self.response = self.client.get(url)
 
     def test_status_code(self):
-        self.assertEquals(self.response.status_code, 200)
+        self.assertEqual(self.response.status_code, 200)
 
     def test_view_function(self):
         view = resolve('/reset/complete/')
-        self.assertEquals(view.func.view_class, auth_views.PasswordResetCompleteView)
+
+        self.assertEqual(view.func.view_class, auth_views.PasswordResetCompleteView)
--- forum/accounts/tests/test_view_signup.py
+++ forum/accounts/tests/test_view_signup.py
@@ -10,20 +10,23 @@ class SignUpTests(TestCase):
 
     def setUp(self):
         url = reverse('signup')
+
         self.response = self.client.get(url)
 
     def test_signup_status_code(self):
-        self.assertEquals(self.response.status_code, 200)
+        self.assertEqual(self.response.status_code, 200)
 
     def test_signup_url_resolves_signup_view(self):
         view = resolve('/signup/')
-        self.assertEquals(view.func, signup)
+
+        self.assertEqual(view.func, signup)
 
     def test_csrf(self):
         self.assertContains(self.response, 'csrfmiddlewaretoken')
 
     def test_contains_form(self):
         form = self.response.context.get('form')
+
         self.assertIsInstance(form, SignUpForm)
 
     def test_form_inputs(self):
@@ -47,22 +50,20 @@ class SuccessfulSignUpTests(TestCase):
         self.home_url = reverse('home')
 
     def test_redirection(self):
-        '''
-        A valid form submission should redirect the user to the home page
-        '''
+        """A valid form submission should redirect the user to the home page."""
         self.assertRedirects(self.response, self.home_url)
 
     def test_user_creation(self):
         self.assertTrue(User.objects.exists())
 
     def test_user_authentication(self):
-        '''
-        Create a new request to an arbitrary page.
-        The resulting response should now have a `user` to its context,
-        after a successful sign up.
-        '''
+        """Create a new request to an arbitrary page.
+
+        The resulting response should now have a `user` to its context, after a successful sign up.
+        """
         response = self.client.get(self.home_url)
         user = response.context.get('user')
+
         self.assertTrue(user.is_authenticated)
 
 
@@ -72,13 +73,12 @@ class InvalidSignUpTests(TestCase):
         self.response = self.client.post(url, {})  # submit an empty dictionary
 
     def test_signup_status_code(self):
-        '''
-        An invalid form submission should return to the same page
-        '''
-        self.assertEquals(self.response.status_code, 200)
+        """An invalid form submission should return to the same page."""
+        self.assertEqual(self.response.status_code, 200)
 
     def test_form_errors(self):
         form = self.response.context.get('form')
+
         self.assertTrue(form.errors)
 
     def test_dont_create_user(self):
